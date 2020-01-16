b0fc181fae478c684f92044d185de32407632d36
PluT00 <valeran2003@mail.ru>
2020-01-17 00:45:03 +0300

    Fix User model Media; Custom User is default

--- Users/models.py
+++ Users/models.py
@@ -1,10 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 
-class MyUser(User):
+class MyUser(AbstractUser):
     avatar = models.ImageField(upload_to='avatar', null=True, blank=True)
-    city = models.CharField(max_length=100)
-    country = models.CharField(max_length=100)
-    bio = models.TextField(max_length=1000)
+    city = models.CharField(max_length=100, blank=True)
+    country = models.CharField(max_length=100, blank=True)
+    bio = models.TextField(max_length=1000, blank=True)
     friends = models.ManyToManyField("self", blank=True)
+
+    def __str__(self):
+        return self.username
