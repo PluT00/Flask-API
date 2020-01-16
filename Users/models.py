837120ae05d1760b329a7cc0493c7b48fcc7f75b
PluT00 <valeran2003@mail.ru>
2020-01-17 00:09:02 +0300

    Add custom User model

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class MyUser(User):
+    avatar = models.ImageField(upload_to='avatar', null=True, blank=True)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    bio = models.TextField(max_length=1000)
+    friends = models.ManyToManyField("self", blank=True)
