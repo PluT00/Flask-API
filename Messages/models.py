76ac1436fd20820ebe4a3d4907eaf4eda3a49e37
PluT00 <valeran2003@mail.ru>
2020-01-18 00:52:23 +0300

    Add Message module

--- Messages/models.py
+++ Messages/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .get_username import get_username
 from Users.models import MyUser
 
 
@@ -7,3 +8,14 @@ class Chat(models.Model):
 
     def __str__(self):
         return ", ".join(p.username for p in self.members.all())
+
+
+class Message(models.Model):
+    message = models.TextField(max_length=10000)
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    author = models.ForeignKey(MyUser, on_delete=models.DO_NOTHING, blank=True)
+    datetime = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.author = get_username().user
+        super(Message, self).save(*args, **kwargs)
