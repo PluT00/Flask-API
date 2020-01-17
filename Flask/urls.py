9b172cdfe44d9f528a5b97d3cb3b945928019bc8
PluT00 <valeran2003@mail.ru>
2020-01-17 23:54:46 +0300

    Move urls routing to Flask app

--- Flask/urls.py
+++ Flask/urls.py
@@ -17,8 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+from Users.views import UserViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('Users.urls'))
+    path('api/', include(router.urls))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
