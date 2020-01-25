27ea92aa38f6ed77879f57d1408d81d75fc34a48
PluT00 <valeran2003@mail.ru>
2020-01-25 11:25:29 +0300

    Update Chat serializer

--- Messages/serializers.py
+++ Messages/serializers.py
@@ -2,13 +2,15 @@ from rest_framework import serializers
 from .models import Chat, Message
 
 
-class ChatSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Chat
-        fields = '__all__'
-
-
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
         fields = '__all__'
+
+
+class ChatSerializer(serializers.ModelSerializer):
+    messages = MessageSerializer(many=True)
+
+    class Meta:
+        model = Chat
+        fields = ['members', 'messages']
