31ede8f8c8cf47e50b224876718a60498cfc7095
PluT00 <valeran2003@mail.ru>
2020-01-29 22:02:28 +0300

    Add Message, Chat views

--- Flask/urls.py
+++ Flask/urls.py
@@ -19,11 +19,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 from Users.views import UserViewSet
+from Messages.views import ChatViewSet, MessageAPIView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'chats', ChatViewSet, 'chats-list')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('api/chats/<int:pk>/post_message/', MessageAPIView.as_view())
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Messages/views.py
+++ Messages/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Chat, Message
+from .serializers import ChatSerializer, MessageSerializer
+from Flask.permissions import IsMember, IsAuthorOrReadOnly
 
-# Create your views here.
+
+class ChatViewSet(ModelViewSet):
+    serializer_class = ChatSerializer
+    permission_classes = [IsMember]
+
+    def get_queryset(self):
+        current_user = self.request.user
+        return Chat.objects.filter(members__exact=current_user)
+
+
+class MessageAPIView(APIView):
+
+    def post(self, request, pk):
+        chat = Chat.objects.get(pk=pk)
+        serializer = MessageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(chat=chat, author=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
