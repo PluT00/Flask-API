a5669da0b8084db5c13bd05802367279fe185da5
PluT00 <valeran2003@mail.ru>
2020-02-01 16:55:47 +0300

    Update Posts models

--- Posts/models.py
+++ Posts/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     text = models.TextField(max_length=10000)
     image = models.ImageField(upload_to='post_images', null=True, blank=True)
     datetime = models.DateTimeField(auto_now=True)
-    likes = models.IntegerField(blank=True, null=True)
+    likes = models.IntegerField(blank=True, default=0)
 
     def __str__(self):
         text = ' '.join(self.text.split(' ')[:4])
@@ -19,5 +19,5 @@ class Comment(models.Model):
     author = models.ForeignKey(MyUser, on_delete=models.CASCADE)
     text = models.TextField(max_length=3000)
     datetime = models.DateTimeField(auto_now=True)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments', blank=True)
 
