34cd9695adfeb849de9572fa668283023a61ca94
PluT00 <valeran2003@mail.ru>
2020-02-09 15:43:42 +0300

    Update User serializer

--- Users/serializers.py
+++ Users/serializers.py
@@ -6,3 +6,10 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = MyUser
         fields = '__all__'
+        read_only_fields = (
+            'is_active', 'is_staff', 'last_login', 'is_superuser', 'date_joined',
+            'user_permissions', 'groups'
+        )
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
