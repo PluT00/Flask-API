eab18126661a68fb6f4aa80af712a8c8e00eda33
PluT00 <valeran2003@mail.ru>
2020-02-09 20:45:16 +0300

    Update Posts serializer

--- Posts/serializers.py
+++ Posts/serializers.py
@@ -1,17 +1,22 @@
 from rest_framework import serializers
+from Users.serializers import UserSerializer
 from .models import Post, Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+
     class Meta:
         model = Comment
         fields = ['id', 'post', 'author', 'text', 'datetime']
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
     comments = CommentSerializer(many=True, required=False)
 
     class Meta:
         model = Post
-        fields = ['id', 'author', 'text', 'image', 'datetime', 'likes', 'comments']
-
+        fields = [
+            'id', 'author', 'text', 'image', 'datetime', 'likes', 'comments'
+        ]
