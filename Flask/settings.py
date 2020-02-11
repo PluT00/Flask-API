2ce08af890dc7f894abf074440abe6cad7229c64
PluT00 <valeran2003@mail.ru>
2020-02-11 23:44:25 +0300

    Update settings.py

--- Flask/settings.py
+++ Flask/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'Users',
     'Messages',
     'Posts',
@@ -133,5 +134,11 @@ AUTH_USER_MODEL = 'Users.MyUser'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 50
+    'PAGE_SIZE': 50,
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 }
